5858899c7538a3e4508b5af2f6b84a752e5976cb
Everly <66821749+everlyy@users.noreply.github.com>
2022-11-15 23:54:51 +0100

    Improve install script

--- install.sh
+++ install.sh
@@ -6,8 +6,23 @@ read -p "Enter upload host url (https://your.website.com/upload.php): " host_url
 
 upload_key=$(echo $RANDOM | md5sum | head -c 32)
 
+read -p "Do you want to set a custom upload key? (Y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	read -p "Enter upload key: " upload_key
+fi
+
 echo "Host URL   : $host_url"
 echo "Upload Key : $upload_key"
+
+read -p "Is the above correct? (Y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Quitting."
+	exit 0
+fi
 
 server_default_config_file=server/config.def.php
 server_config_file=server/config.php
@@ -16,7 +31,7 @@ client_config_path=~/.config/file-uploader
 client_config_file=$client_config_path/config.ini
 
 client_upload_script=client/upload.py
-client_script_destination=/usr/bin
+client_script_destination=/usr/bin/upload-file
 
 context_menu_source=client/kde-plasma-context-menu/Upload.desktop
 context_menu_destination=/usr/share/kservices5/ServiceMenus
@@ -36,9 +51,9 @@ echo "uploadkey = $upload_key" >> $client_config_file
 
 echo "Copying $client_upload_script to $client_script_destination"
 
-sudo cp $client_upload_script $client_script_destination/upload-file
+sudo cp $client_upload_script $client_script_destination
 
-read -p "Do you want to install the context menu entry for Dolphin? " -n 1 -r
+read -p "Do you want to install the context menu entry for Dolphin? (Y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
